734dbf66d4b0ddeffaedcdc71e55e3f490e364cd
Nikhil <nikhilsingh675naman@gmail.com>
2025-05-02 13:02:36 +0530

    changes

--- App.js
+++ App.js
@@ -7,11 +7,20 @@ const { createOrder, verifyPayment } = require("./controllers/RazorController");
 const RegisterRoutes = require("./controllers/RegisterController");
 const LoginRoutes = require("./controllers/LoginController");
 
-// Initialize express app
+
 const app = express();
 
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "https://sedulous-charity-and-crowdfunding.vercel.app"  
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 // Middleware setup
-app.use(cors()); // Enable CORS
 app.use(express.json()); // Parse JSON body
 
 // Connect to MongoDB
@@ -21,7 +30,6 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Backend is live! Maa Siddheshwari Charity API is running.");
 });
-
 
 // Routes
 app.post("/api/donations", donatingRoute); // Donation API route
@@ -34,5 +42,6 @@ app.post("/api/login", LoginRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+ 
